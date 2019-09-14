76a6711abd0a248bd897f5887417d94cd38737a4
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-09-14 16:41:35 -0400

    Adds Delete button callback prop

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import List from 'List';
+import React, { Component } from 'react';
+import List from './List'
 import './App.css';
 
-
 class App extends Component {
-  static defaultProps = {
+  state = {
     store: {
       lists: [],
       allCards: {},
     }
   };
-
+  handleDeleteItem(cards){
+    console.log('handle delete item called', {cards})
+  }
+  handleAddItem(){
+    console.log('handle add item called')
+  }
   render() {
     const { store } = this.props
-  return (
-    <main className='App'>
-      <header class="App-header">
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className="App-list">
-        {props.list.map((list)=>
-        <List
-        key={list.id}
-        header={list.header}
-        cards={list.cardIds.map(id => store.allCards[id])}
-        />
-        )}
-      </div>
-    </main>
-  )
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+              onDeleteItem={this.handleDeleteItem}
+              onAddItem={this.handleAddItem}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import Card from 'Card';
+import Card from './Card'
+import './List.css';
 
-function list(props){
-    return(
-        <section class="List">
-            <header class="List-header">
-                <h2>{props.header}</h2>
-            </header>
-            <div class="List-cards">
-                {props.card.map((card)=>
-                    <Card
-                    key ={card.id}
-                    title = {card.title}
-                    content = {card.content}
-                    />
-                )}
-                <button
-                    type='button'
-                    className='List-add-button'
-                >
-                    + Add Random Card
-                </button>
-            </div>
-        </section>
-    )
+export default function List(props) {
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.cards.map((card) =>
+          <Card
+            key={card.id}
+            title={card.title}
+            content={card.content}
+            onDeleteItem={props.onDeleteItem}
+            onAddItem={props.onAddItem}
+          />
+        )}
+        <button
+          type='button'
+          className='List-add-button'
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
 }
